ac63d3ee4e1f9c6d122de9d2e7bb3515a7c53df2
MirotiDV <miguelroldanmart2001@outlook.es>
2025-08-22 20:16:15 +0200

    buttons added

--- src/components/ListOfUsers.tsx
+++ src/components/ListOfUsers.tsx
@@ -1,5 +1,4 @@
 import {
-  Badge,
   Card,
   Table,
   TableBody,
@@ -7,83 +6,94 @@ import {
   TableHead,
   TableHeaderCell,
   TableRow,
-} from '@tremor/react';
+} from "@tremor/react";
 
 const data = [
   {
-    name: 'Viola Amherd',
-    Role: 'Federal Councillor',
-    departement:
-      'The Federal Department of Defence, Civil Protection and Sport (DDPS)',
-    status: 'active',
+    id: 1,
+    name: "Miguel Ángel",
+    email: "Miguel@gmail.com",
+    github: "MirotiDV",
   },
   {
-    name: 'Albert Rösti',
-    Role: 'Federal Councillor',
-    departement:
-      'The Federal Department of the Environment, Transport, Energy and Communications (DETEC)',
-    status: 'active',
+    id: 2,
+    name: "Alex Merc",
+    email: "Alex@gmail.com",
+    github: "AlexMR",
   },
   {
-    name: 'Beat Jans',
-    Role: 'Federal Councillor',
-    departement: 'The Federal Department of Justice and Police (FDJP)',
-    status: 'active',
-  },
-  {
-    name: 'Ignazio Cassis',
-    Role: 'Federal Councillor',
-    departement: 'The Federal Department of Foreign Affairs (FDFA)',
-    status: 'active',
-  },
-  {
-    name: 'Karin Keller-Sutter',
-    Role: 'Federal Councillor',
-    departement: 'The Federal Department of Finance (FDF)',
-    status: 'active',
-  },
-  {
-    name: 'Guy Parmelin',
-    Role: 'Federal Councillor',
-    departement:
-      'The Federal Department of Economic Affairs, Education and Research (EAER)',
-    status: 'active',
-  },
-  {
-    name: 'Elisabeth Baume-Schneider',
-    Role: 'Federal Councillor',
-    departement: 'The Federal Department of Home Affairs (FDHA)',
-    status: 'active',
+    id: 3,
+    name: "Logan Moth",
+    email: "Logan@gmail.com",
+    github: "LoganMT",
   },
 ];
 
 export function ListOfUsers() {
   return (
     <Card>
-      <h3 className="text-tremor-content-strong dark:text-dark-tremor-content-strong font-semibold">List of Swiss Federal Councillours</h3>
-      <Table className="mt-5">
+      <h3>Lista de usuarios</h3>
+      <Table>
         <TableHead>
           <TableRow>
-            <TableHeaderCell>Name</TableHeaderCell>
-            <TableHeaderCell>Position</TableHeaderCell>
-            <TableHeaderCell>Department</TableHeaderCell>
-            <TableHeaderCell>Status</TableHeaderCell>
+            <TableHeaderCell>id</TableHeaderCell>
+            <TableHeaderCell>Nombre</TableHeaderCell>
+            <TableHeaderCell>Email</TableHeaderCell>
+            <TableHeaderCell>Acciones</TableHeaderCell>
           </TableRow>
         </TableHead>
         <TableBody>
           {data.map((item) => (
-            <TableRow key={item.name}>
-              <TableCell>{item.name}</TableCell>
-              <TableCell>
-                {item.Role}
+            <TableRow key={item.id}>
+              <TableCell>{item.id}</TableCell>
+              <TableCell style={{ display: "flex", alignItems: "center" }}>
+                <img
+                  style={{
+                    width: "32px",
+                    height: "32px",
+                    borderRadius: "50%",
+                    marginRight: "8px",
+                  }}
+                  src={`https://unavatar.io/github/${item.github}`}
+                  alt={item.name}
+                />
+                {item.name}
               </TableCell>
+              <TableCell>{item.email}</TableCell>
               <TableCell>
-                {item.departement}
-              </TableCell>
-              <TableCell>
-                <Badge color="emerald">
-                  {item.status}
-                </Badge>
+                <button type="button">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={1.5}
+                    stroke="currentColor"
+                    className="size-6"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10"
+                    />
+                  </svg>
+                </button>
+
+                <button type="button">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={1.5}
+                    stroke="currentColor"
+                    className="size-6"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="m9.75 9.75 4.5 4.5m0-4.5-4.5 4.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+                    />
+                  </svg>
+                </button>
               </TableCell>
             </TableRow>
           ))}
